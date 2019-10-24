812a7ce83d3541cadd372108110531db17d5ec0e
Mike Hunsicker <hunsickm@matc.edu>
2019-10-23 20:42:04 -0500

    Completed Activity 6

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -38,5 +38,56 @@ namespace API.Controllers
     {
       return this.context.Posts.Find(id);
     }
+
+    /// <summary>
+    /// POST api/post
+    /// </summary>
+    /// <param name="request">JSON request containing post fields</param>
+    /// <returns>A new post</returns>
+    [HttpPost]
+    public ActionResult<Post> Create([FromBody]Post request)
+    {
+      var post = new Post
+      {
+        Id = request.Id,
+        Title = request.Title,
+        Body = request.Body,
+        Date = request.Date
+      };
+
+      context.Posts.Add(post);
+      var success = context.SaveChanges() > 0;
+
+      if (success)
+      {
+        return post;
+      }
+
+      throw new Exception("Error creating post");
+    }
+
+    public ActionResult<Post> Update([FromBody]Post request)
+    {
+      var post = context.Posts.Find(request.Id);
+
+      if (post == null)
+      {
+        throw new Exception("Could not find post");
+      }
+
+      // Update the post properties with request values, if present.
+      post.Title = request.Title != null ? request.Title : post.Title;
+      post.Body = request.Body != null ? request.Body : post.Body;
+      post.Date = request.Date != null ? request.Date : post.Date;
+
+      var success = context.SaveChanges() > 0;
+
+      if (success)
+      {
+        return post;
+      }
+
+      throw new Exception("Error updating post");
+    }
   }
 }
